de3d3b29ba767cbaddc88030ff17a47a13794c42
Peter Novassat <novassatsystems@gmail.com>
2025-06-12 13:09:17 -0300

    Update Sample

---
+++ Sample/Maui.DynamicIcon.Sample/SingleUseStrategy.cs
@@ -0,0 +1,8 @@
+﻿namespace Maui.DynamicIcon.Sample;
+
+public class SingleUseStrategy(bool mustBeChange) : IDynamicIconStrategy
+{
+
+    private readonly string _alias = mustBeChange ? "Icon1" : "Icon2";
+    public string? GetNextIconAlias() => _alias;
+}
